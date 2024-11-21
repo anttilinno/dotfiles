5e6a8edd3d884f6abda0ce42151ded954fdb9b04
Antti <antti.linno@gmail.com>
2024-11-21 15:05:02 +0200

    Add polybar and remove nvim for time being

--- dot_config/nvim/lua/plugins/gruvbox.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "ellisonleao/gruvbox.nvim",
-  name = "gruvbox",
-  priority = 1000,
-  config = true,
-}
--- dot_config/nvim/lua/plugins/vim-surround.lua
+++
@@ -1,4 +0,0 @@
-return {
-  "tpope/vim-surround",
-  keys = { "d", "c", "y" },
-}
